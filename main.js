2e0acc34366b367681e161a296a04fe882e5e7bd
Santiago <santiagomerlo69@gmail.com>
2023-11-02 19:20:50 -0300

    nueva version

---
+++ main.js
@@ -0,0 +1,22 @@
+// Variables
+let equipoNombre = "Mi Equipo de Fútbol";
+var numeroJugadores = 11;
+const formacion = "4-4-2";
+
+// Funciones
+function saludarEquipo() {
+    console.log("Hola a todos los jugadores del equipo");
+}
+
+// Condicionales
+if (numeroJugadores >= 11) {
+    console.log("Tenemos un equipo completo.");
+} else {
+    console.log("Necesitamos más jugadores.");
+}
+
+// Eventos
+console.log("Bienvenido")
+alert("Bienvendo")
+
+
